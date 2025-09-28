98ccd91fadda695999acca9404ff0026f9c981c4
hazemhosam <hazemhosam105@gmail.com>
2025-09-28 19:15:57 +0300

    feat: creating the Market_research_report

--- src/markting/crew.py
+++ src/markting/crew.py
@@ -3,15 +3,23 @@ from crewai.project import CrewBase, agent, task, crew
 from crewai_tools import (SerperDevTool, ScrapeWebsiteTool, DirectoryReadTool, FileReadTool,
                           FileWriterTool)
 from crewai.agents.agent_builder.base_agent import BaseAgent
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv 
 from typing import List
 from datetime import datetime
-import os
 
 _ = load_dotenv() 
 
 llm = LLM("gemini/gemini-2.0-flash",
           temperature=.3) 
+
+class Content(BaseModel):
+    content_type: str = Field(...,
+                              description="The type of content to be created (e.g., blog post, social media post, video)")
+    topic: str = Field(..., description="The topic of the content")
+    target_audience: str = Field(..., description="The target audience for the content")
+    tags: List[str] = Field(..., description="Tags to be used for the content")
+    content: str = Field(..., description="The content itself")
 
 @CrewBase
 class MarkteingCrew():
@@ -28,9 +36,63 @@ class MarkteingCrew():
             DirectoryReadTool(directory_path="./resources/drafts"),
             FileReadTool(),
             FileWriterTool()
-        ],  max_iter=5,
+        ],  max_iter=30,
             max_rpm=3,
             verbose=True)
+        
+    @agent
+    def content_creator_social_media(self) -> Agent:
+        return Agent(
+            config=self.agents_config['content_creator_social_media'],#type: ignore
+            tools=[
+                SerperDevTool(),
+                ScrapeWebsiteTool(),
+                DirectoryReadTool('resources/drafts'),
+                FileWriterTool(),
+                FileReadTool()
+            ],
+            inject_date=True,
+            llm=llm,
+            allow_delegation=True,
+            max_iter=30,
+            max_rpm=3
+        )
+
+    @agent
+    def content_writer_blogs(self) -> Agent:
+        return Agent(
+            config=self.agents_config['content_writer_blogs'],#type: ignore
+            tools=[
+                SerperDevTool(),
+                ScrapeWebsiteTool(),
+                DirectoryReadTool('resources/drafts/blogs'),
+                FileWriterTool(),
+                FileReadTool()
+            ],
+            inject_date=True,
+            llm=llm,
+            allow_delegation=True,
+            max_iter=5,
+            max_rpm=3
+        )
+
+    @agent
+    def seo_specialist(self) -> Agent:
+        return Agent(
+            config=self.agents_config['seo_specialist'],#type: ignore
+            tools=[
+                SerperDevTool(),
+                ScrapeWebsiteTool(),
+                DirectoryReadTool('resources/drafts'),
+                FileWriterTool(),
+                FileReadTool()
+            ],
+            inject_date=True,
+            llm=llm,
+            allow_delegation=True,
+            max_iter=3,
+            max_rpm=3
+        )
      
     @task
     def market_research(self) -> Task:
@@ -38,6 +100,60 @@ class MarkteingCrew():
             config=self.tasks_config['market_research'], #type: ignore
             agent=self.marketing_agent() #type: ignore
          )
+    
+    @task
+    def marketing_strategy(self) -> Task:
+        return Task(
+            config=self.tasks_config['marketing_strategy'],#type: ignore
+            agent=self.marketing_agent()
+        )
+
+    @task
+    def create_content_calendar(self) -> Task:
+        return Task(
+            config=self.tasks_config['create_content_calendar'],#type: ignore
+            agent=self.content_creator_social_media()
+        )
+
+    @task
+    def prepare_post_drafts(self) -> Task:
+        return Task(
+            config=self.tasks_config['prepare_post_drafts'],#type: ignore
+            agent=self.content_creator_social_media(),
+            output_json=Content
+        )
+
+    @task
+    def prepare_scripts_for_reels(self) -> Task:
+        return Task(
+            config=self.tasks_config['prepare_scripts_for_reels'],#type: ignore
+            agent=self.content_creator_social_media(),
+            output_json=Content
+        )
+
+    @task
+    def content_research_for_blogs(self) -> Task:
+        return Task(
+            config=self.tasks_config['content_research_for_blogs'],#type: ignore
+            agent=self.content_writer_blogs()
+        )
+
+    @task
+    def draft_blogs(self) -> Task:
+        return Task(
+            config=self.tasks_config['draft_blogs'],#type: ignore
+            agent=self.content_writer_blogs(),
+            output_json=Content
+        )
+
+    @task
+    def seo_optimization(self) -> Task:
+        return Task(
+            config=self.tasks_config['seo_optimization'],#type: ignore
+            agent=self.seo_specialist(),
+            output_json=Content
+        )
+            
     @crew
     def crew(self) -> Crew:
         """Creates the content writing crew"""
@@ -53,7 +169,7 @@ inputs = {
         "product_name": "AI Powered Excel Automation Tool",
         "target_audience": "Small and Medium Enterprises (SMEs)",
         "product_description": "A tool that automates repetitive tasks in Excel using AI, saving time and reducing errors.",
-        "budget": "Rs. 50,000",
+        "budget": "15,000 $",
         "current_date": datetime.now().strftime("%Y-%m-%d")}
 crew = MarkteingCrew()
 crew.crew().kickoff(inputs) 
